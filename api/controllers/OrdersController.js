9383a4cbc9123bbd1eb04ea324976d151293674b
Dhilip <dhilip@thinkcodesoft.com>
2020-05-05 14:44:00 +0530

    Option to download order reports.

--- api/controllers/OrdersController.js
+++ api/controllers/OrdersController.js
@@ -9,10 +9,12 @@
 let _ = require('lodash');
 let path = require('path');
 let XLSX = require('xlsx');
+let fs = require('fs');
+let async = require('async');
 let filePath = path.resolve(__dirname + '/../../order_report.csv');
 
-
 module.exports = {
+    
     getAllOrders: async (req, res) => {
         try {
             let allOrders = await Orders.find();
@@ -71,23 +73,80 @@ module.exports = {
         if (!err) console.log('Success.')
       });
     },
-  consolidatedOrdersCSV: async (req, res) => {
-    let consolidatedOrders = await Orders.consolidateOrders();
-    let workSheet = XLSX.utils.json_to_sheet(consolidatedOrders, {dateNF: 'yyyy-mm-dd@'});
-    let workBook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(workBook, workSheet, 'consolidated_orders');
-    XLSX.writeFile(workBook, filePath);
 
-    if(fs.existsSync(filePath)) {
-      res.setHeader('Content-disposition', 'attachment;filename=' + 'test_reports.csv');
-      var fileStream = fs.createReadStream(filePath);
-      fileStream.pipe(res);
-    } else {
-      res.json({
-        err: 'File Not Found'
-      })
+    consolidatedOrdersCSV: async (req, res) => {
+      let consolidatedOrders = await Orders.consolidateOrders();
+      let workSheet = XLSX.utils.json_to_sheet(consolidatedOrders, {dateNF: 'yyyy-mm-dd@'});
+      let workBook = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(workBook, workSheet, 'consolidated_orders');
+      XLSX.writeFile(workBook, filePath);
+
+      if(fs.existsSync(filePath)) {
+        res.setHeader('Content-disposition', 'attachment;filename=' + 'test_reports.csv');
+        var fileStream = fs.createReadStream(filePath);
+        fileStream.pipe(res);
+      } else {
+        res.json({
+          err: 'File Not Found'
+        })
+      }
+    },
+
+    downloadOrderReport: async (req, res) => {
+
+      try {
+        
+        let recentOrders = await Orders.find({ orderStatus: "Order Placed" });
+        let orderReports = [];
+        
+        async.forEachOf(recentOrders, (order, key, cb) => {
+          
+          var orderDetails = {
+            'OrderId': order.orderId,
+            'Name': order.userName,
+            'Mobile': order.userMobileNo,
+            'OrderDate': order.orderDate,
+            'Orders': '',
+            'Total Price': order.finalPrice
+          };
+
+          async.forEachOf(order.items, (orderItem, key, innercb) => {
+            let items = orderItem['product-name'] + ' - ' + orderItem['product-quantity'] + ' ' + orderItem['product-price'];
+            if (key < order.items.length - 1) {
+              items = items + ', '; 
+            }
+            orderDetails['Orders'] += items;
+            innercb(); 
+          },
+          () => {
+            orderReports.push(orderDetails);
+            cb();
+          });
+        }, 
+        () => {
+
+          var workSheet = XLSX.utils.json_to_sheet(orderReports, {dateNF: 'yyyy-mm-dd@'});
+          var workBook = XLSX.utils.book_new();
+
+          XLSX.utils.book_append_sheet(workBook, workSheet, 'consolidated_orders');
+          XLSX.writeFile(workBook, filePath);
+
+          if (fs.existsSync(filePath)) {
+            res.setHeader('Content-disposition', 'attachment; filename=' + 'consolidated_order_reports.csv');
+            var filestream = fs.createReadStream(filePath);
+            filestream.pipe(res);
+          }
+          else {
+            return res.json({error : "File not Found"});
+          }
+        });
+      } 
+      catch (error) {
+        return res.serverError({
+          status: 500,
+          msg: error
+        });
+      }
     }
-
-  }
 };
 
